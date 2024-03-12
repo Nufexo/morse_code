fc4aa62c54a52700c481f69964c174b52d8e4ee9
Nufexo <antgranada@gmail.com>
2024-03-12 12:04:06 +0100

    Adding sound

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 from vocabulary import morse_language
+from pydub import AudioSegment
+from pydub.playback import play
 
 
 def convert(text: str):
-    output = []
-    for word in range(len(text)):
+    output: list = []
+    for word in text:
         for letter in word:
             new_letter = morse_language[letter]
-            output[word].append(new_letter)
+            output.append(new_letter)
 
     return output
 
 
+def reproduce(output):
+    dot = AudioSegment.from_wav("sounds/dot.wav")
+    dash = AudioSegment.from_wav("sounds/dash1.wav")
+    for word in output:
+        for simbol in word:
+            if simbol == "▄":
+                play(dot)
+
+
 text: str = input("Type here your text: ").lower()
-print(convert(text=text))
+output = convert(text=text)
+print("".join(output))
+reproduce(output=output)
