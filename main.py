0dd8d7c9aeb5c1419edf6acda3348c96487c7b6e
Nufexo <antgranada@gmail.com>
2024-03-12 12:08:18 +0100

    finish

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from vocabulary import morse_language
 
 def convert(text: str):
     output = []
-    for word in range(len(text)):
+    for word in text:
         for letter in word:
             new_letter = morse_language[letter]
-            output[word].append(new_letter)
+            output.append(new_letter)
 
     return output
 
 
 text: str = input("Type here your text: ").lower()
-print(convert(text=text))
+output = convert(text=text)
+print("".join(output))
